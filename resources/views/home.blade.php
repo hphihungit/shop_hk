366b681555393c255347830db49a0ff44f6d2ad3
Tuan Kiet <kiethdt.22ite@vku.udn.vn>
2023-11-11 21:54:57 +0700

    Add background-image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 
             @foreach($sliders as $slider)
 
-            <div class="item-slick1">
+            <div class="item-slick1" style="background-image: url('{{ $slider->thumb }}');">
                 <div class="container h-full">
                     <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
                         <div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
